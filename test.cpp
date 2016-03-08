bb9f4009ecffd42a6e9708f3d5e5e86e40466cf6
ChocoKing <lan.trinh@lazada.com>
2016-03-08 23:04:05 +0700

    Added files via upload

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	cout<<"My name is Lan.\n";
+	
+	int i=1;
+	int sum=0;
+	for (i;i<=1000;i++)
+	    sum=sum+i;
+	cout<<"Sum up the first 1000 numbers: ";
+	cout<<sum;
+	return 0;
+}
